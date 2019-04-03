d81b1ed579b97c3f527d71cef3677eab9c339011
SauravRana <sauravrana1983@hotmail.com>
2019-04-03 11:16:42 +0530

    Initial Documentation

---
+++ Challenge/Introductory/9_Merge_Tool.py
@@ -0,0 +1,35 @@
+# Consider the following:
+# A string, , of length  where .
+# An integer, , where  is a factor of .
+# We can split  into  subsegments where each subsegment, , consists of a contiguous block of  characters in . Then, use each  to create string  such that:
+# The characters in  are a subsequence of the characters in .
+# Any repeat occurrence of a character is removed from the string such that each character in  occurs exactly once. In other words, if the character at some index  in  occurs at a previous index  in , then do not include the character in string .
+# Given  and , print  lines where each line  denotes string .
+# https://www.hackerrank.com/challenges/merge-the-tools/problem?h_r=next-challenge&h_v=zen
+
+# Sample Input ==> AABCAAADA  3 
+# Sample Output ==> 
+# AB
+# CA
+# AD
+
+# The purpose of zip() is to map the similar index of multiple containers so that they can be used just using as single entity.
+# Syntax : 
+# zip(*iterators)
+# Parameters : 
+# Python iterables or containers ( list, string etc )
+# Return Value : 
+# Returns a single iterator object, having mapped values from all the
+# containers.
+
+# def remove_duplicates(data):
+#     for i in data:
+
+def string_split(string, length):
+    print([string[i:i+3] for i in range(0, len(string), length)])
+    for t in [string[i:i+length] for i in range(0, len(string), length)]:   #using the range function to get the substring and string out
+        print(t)
+    for part in zip(*[iter(string)]*length):
+        print(part)
+
+string_split('AABCAAADA', 3)
---
+++ Challenge/Introductory/readme.md
@@ -0,0 +1,14 @@
+#Python Zip Function
+---
+
+The purpose of zip() is to map the similar index of multiple containers so that they can be used just using as single entity.
+Unzipping means converting the zipped values back to the individual self as they were. This is done with the help of “*” operator.
+
+
+##Syntax : 
+zip(*iterators)
+##Parameters : 
+Python iterables or containers ( list, string etc )
+##Return Value : 
+Returns a single iterator object, having mapped values from all the
+containers.
